3e92e07a8b2b8b93773553f3118ca8adf4c1fe97
HyunAh Kim <wgrape0@gmail.com>
2021-02-21 23:26:01 +0900

    Update MainActivity.kt

--- Android- TimerEdt/MainActivity.kt
+++ Android- TimerEdt/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.rndns.timeredt
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.*
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var recordlist: ListView
@@ -18,19 +23,29 @@ class MainActivity : AppCompatActivity() {
         recordlist = findViewById(R.id.recordlist)
         startbtn = findViewById(R.id.startbtn)
         showRecord = findViewById(R.id.showRecord)
+
+        // 클릭하면, TimerActivity로 이동
         startbtn.setOnClickListener {
-            // Go next page
             val intent = Intent(this, TimerActivity::class.java)
             startActivity(intent)
         }
+        // 클릭하면, DB를 연결하여 저장된 List<TimerObj> 출력
         showRecord.setOnClickListener {
             db = TimerDatabase.getInstance(this)
             var timerlist: List<TimerObj>? = db?.TimerObjDao()?.getAll()
 
-            System.out.println("  [showRecord clicked] > ")
-            System.out.println("       - timerlist: "+timerlist.toString())
+            // Log를 통해 확인
+            Log.d("MA10001", " - is DB open: "+db?.isOpen)
+            Log.d("MA10001", " - timer list: "+timerlist.toString())
 
-            // recordlist.adapter // Hmmm..
+            // 화면 상단의 리스트뷰에서 확인
+            if (!timerlist?.isEmpty()!!) {
+                val adapter = ArrayAdapter<TimerObj>(this, android.R.layout.simple_list_item_1, timerlist!!)
+                recordlist.setAdapter(adapter)
+                adapter.notifyDataSetChanged()
+            } else {
+                Toast.makeText(this, "There's no timer data!", Toast.LENGTH_LONG)
+            }
         }
     }
 }
